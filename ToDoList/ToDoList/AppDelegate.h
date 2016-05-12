a8de2ab444428d85f68a7b81139c66901347da73
SAP <shevtsov.ap@gmail.com>
2016-05-12 21:28:30 +0300

    create project with initial file structure

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Andrey on 5/12/16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Andrey on 5/12/16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
