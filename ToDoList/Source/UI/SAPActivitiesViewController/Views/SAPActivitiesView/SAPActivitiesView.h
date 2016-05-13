898f64838279e45e04756ffd04217a201ea3dafc
SAP <shevtsov.ap@gmail.com>
2016-05-13 11:35:32 +0300

    SAPActivitiesView tableView IBOutlet

--- ToDoList/Source/UI/SAPActivitiesViewController/Views/SAPActivitiesView/SAPActivitiesView.h
+++ ToDoList/Source/UI/SAPActivitiesViewController/Views/SAPActivitiesView/SAPActivitiesView.h
@@ -5,9 +5,11 @@
 //  Created by Andrey on 5/12/16.
 //  Copyright © 2016 Andrey. All rights reserved.
 //
+#import <UIKit/UITableView.h>
 
 #import "SAPView.h"
 
 @interface SAPActivitiesView : SAPView
+@property (nonatomic, strong) IBOutlet UITableView  *tableView;
 
 @end
