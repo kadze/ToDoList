85f18bd7b738c631fe3bb4b74a79b75708929e51
SAP <shevtsov.ap@gmail.com>
2016-05-16 01:45:00 +0300

    SAPBundleImageModel, images: call, task, meeting

---
+++ ToDoList/Source/Library/Classes/Models/SAPImageModel/SAPBundleImageModel/SAPBundleImageModel.h
@@ -0,0 +1,13 @@
+//
+//  SAPBundleImageModel.h
+//  ToDoList
+//
+//  Created by Andrey on 5/16/16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import "SAPImageModel.h"
+
+@interface SAPBundleImageModel : SAPImageModel
+
+@end
